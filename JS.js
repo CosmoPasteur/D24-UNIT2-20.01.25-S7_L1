a0c05d396282aaef5a5e1b9186c3b9f4a633ec97
“Cosimo <pastore_cosimo@yahoo.it>
2025-01-25 14:10:36 +0100

    creazione classe pg1

--- JS.js
+++ JS.js
@@ -1,6 +1,12 @@
-let User = {
-  firstName: "",
-  lastName: "",
-  age: "",
-  location: "",
-};
+const form = document.getElementById("formShop"); //abbiamo il nodo del form (è un oggetto js)
+
+class User {
+  constructor(firstName, lastName, age, location) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.location = location;
+  }
+
+  confront;
+}
